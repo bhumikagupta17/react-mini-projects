45ff38b2c6f3bcd89af3ecf35e9094c37b6f7e29
Bhumika <bhumika17gupta@gmail.com>
2026-04-28 14:51:49 +0530

    completed To Do Application

--- 07-toDoList/src/App.jsx
+++ 07-toDoList/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import './index.css'
 import { ToDoProvider } from './contexts';
+import { useEffect } from 'react';
+import { TodoForm, TodoItem } from './components';
 function App() {
 
   const [todos, setTodos] = useState([]);//---> todos is an array with multiple todo
@@ -12,9 +14,9 @@ function App() {
   }
 
   const updateTodo=(id,todo)=>{
-    setTodos((prev)=>prev.map((
+    setTodos((prev)=>prev.map((prevToDo)=>
       prevToDo.id===id ? todo : prevToDo
-    )))
+    ))
   }
 
   const deleteTodo=(id)=>{
@@ -26,17 +28,33 @@ function App() {
     setTodos((prev)=>prev.map((prevToDo)=>
     prevToDo.id===id?{...prevToDo,completed:!prevToDo.completed}:prevToDo))
   }
+  useEffect(() => {
+    const todos=JSON.parse(localStorage.getItem("todos"))
+    if(todos && todos.length>0){
+      setTodos(todos)
+    }
+  }, []);
 
+  useEffect(() => {
+    localStorage.setItem("todos",JSON.stringify(todos))
+  }, [todos]);
+  
   return (
     <ToDoProvider value={{todos,addToDo,updateTodo,deleteTodo,toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
             <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
             <div className="mb-4">
-                {/* Todo form goes here */} 
+                <TodoForm/>
             </div>
             <div className="flex flex-wrap gap-y-3">
                 {/*Loop and Add TodoItem here */}
+                {todos.map((todo)=>(
+                  <div key={todo.id}
+                  className='w-full'>
+                    <TodoItem todo={todo}/>
+                  </div>
+                ))}
             </div>
         </div>
     </div>
